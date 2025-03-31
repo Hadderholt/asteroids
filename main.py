2f36c4758c0f88ceca1fc203719ff1a0d883ab74
Harrod <harrod.adderholt@gmail.com>
2025-03-30 22:31:01 -0500

    Added movement and rotation

--- main.py
+++ main.py
@@ -22,10 +22,14 @@ def main():
             if event.type == pygame.QUIT:
                 return
         
-        screen.fill((0, 0, 0))
+        
+		dt = clock.tick(60) / 1000
+        player.update(dt)
+        
+		screen.fill((0, 0, 0))
         player.draw(screen)  # Now you can use the player variable
         pygame.display.flip()
-        dt = clock.tick(60) / 1000
+        
 
 
 if __name__ == "__main__":
